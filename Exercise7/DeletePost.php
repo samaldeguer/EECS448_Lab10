86bb8949232b6ad1b4b46e863d4d6004c36df8a5
Sam Aldeguer <aldeguersam@ku.edu>
2022-05-05 02:42:58 -0500

    Implement Exercise 7

--- Exercise7/DeletePost.php
+++ Exercise7/DeletePost.php
@@ -1,1 +1,26 @@
+<?php
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "s661a552", "ohz3heet", "s661a552");
+  if ($mysqli->connect_errno)
+  {
+      printf("Connect failed: %s\n", $mysqli->connect_error);
+      exit();
+  }
+  else
+  {
+    echo "<h4>Deleted post id's:</h4>";
+    $query = "SELECT * From Posts";
+    $result = mysqli_query($mysqli, $query);
+    if (isset($_POST['delete']))
+    {
+      if (isset($_POST['checkbox']))
+        foreach ($_POST['checkbox'] as $delete)
+        {
+          $query = "DELETE FROM Posts WHERE post_id=".$delete;
+          mysqli_query($mysqli, $query);
+          echo "<p>".$delete."</p>";
+        }
 
+    }
+  }
+  $mysqli->close();
+?>
